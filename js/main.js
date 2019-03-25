3ee7dacd553e3ef1fcdd7a41bd36d805d67a5234
Le <t_le86524@tss-hr420-dm20.local>
2019-03-25 16:47:43 -0400

    animation with timelite

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 (() => {
 
-	let badge = document.querySelector('img');
+	let myTL = new TimelineLite();
 
-	function animImage() {
-		TweenMax.to(badge, 0.7, {scaleX: 1.025, scaleY: 1.025, rotation: 50, ease:Elastic.easeInOut, onComplete: animFooterText });
-	}
-
-	function animFooterText() {
-		TweenMax.to('h3', 0.7, {scaleX: 2, scaleY: 2, ease:Elastic.easeOut});
-	}
-	// animImage();
-	// 
-	badge.addEventListener('mouseover', animImage);
+	myTL.to("#background", 0.5, {rotation: 180, scaleX: 1.1, scaleY: 1.1, transformOrigin: "50% 50%"}).to("#small_type", 1, {rotation: -360, transformOrigin: "50% 50%"});
 
 })();
